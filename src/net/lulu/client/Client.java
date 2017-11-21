f76848bbbc518cecfd7b3211e3bb0c14ea4eecc3
Lulu Roth <ls80zyse@studserv.uni-leipzig.de>
2017-11-21 22:48:18 +0100

    create client and server classes
    I have created a client and a server class (taken from the Java KnockKnockClient tutorial).
    The server functions as an echo server which just echos the input from the client until the client passes the string "bye".

---
+++ src/net/lulu/client/Client.java
@@ -0,0 +1,35 @@
+package net.lulu.client;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try (
+                Socket socket = new Socket(hostName, portNumber);
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream())
+                )
+        ) {
+            String fromServer, fromUser;
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            while((fromServer = in.readLine()) != null) {
+                System.out.println("Server: " + fromServer);
+                if(fromServer.equals("bye"))
+                    break;
+
+                fromUser = stdIn.readLine();
+                if (fromUser != null) {
+                    System.out.println("Client: " + fromUser);
+                    out.println(fromUser);
+                }
+            }
+        }
+    }
+}
---
+++ src/net/lulu/server/Server.java
@@ -0,0 +1,38 @@
+package net.lulu.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main (String args[]) throws IOException {
+        int portNumber = Integer.parseInt(args[0]);
+
+        try (
+                ServerSocket serverSocket = new ServerSocket(portNumber);
+                Socket clientSocket = serverSocket.accept();
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream())
+                )
+        ){
+            String inputLine, outputLine;
+            // Initiate conversation with client
+            outputLine = "Test-Protocol";
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = inputLine;
+                out.println(outputLine);
+                if (outputLine.equalsIgnoreCase("bye"))
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
